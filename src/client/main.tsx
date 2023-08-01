22a033326858399799616ace7a82ce817c3b48ce
Pauline Thomas <p.thomas@wildsense.co>
2023-08-01 10:47:01 +0200

    delete dead code

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -10,56 +10,6 @@ import ReactDOM from "react-dom";
 import { AuthProviderWrapper } from "./context/auth.context";
 
 function App() {
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <ContentWrapper />,
-  //     errorElement: <NotFound />,
-  //     children: [
-  //       {
-  //         path: "/",
-  //         element: <LandingPage />,
-  //         errorElement: <NotFound />,
-  //       },
-  //       {
-  //         path: "auth/login",
-  //         element: <LoginPage />,
-  //         errorElement: <NotFound />,
-  //       },
-  //       {
-  //         path: "auth/register",
-  //         element: <RegisterPage />,
-  //         errorElement: <NotFound />,
-  //       },
-  //       {
-  //         path: "/trips",
-  //         element: <HomePage />,
-  //         errorElement: <NotFound />,
-  //       },
-  //       {
-  //         path: "/trips/:id",
-  //         element: <TripDetailsPage />,
-  //         errorElement: <NotFound />,
-  //       },
-  //       {
-  //         path: "/trips/create",
-  //         element: <CreateTripPage />,
-  //         errorElement: <NotFound />,
-  //       },
-  //       {
-  //         path: "/user/:id",
-  //         element: <ProfilePage />,
-  //         errorElement: <NotFound />,
-  //       },
-  //       {
-  //         path: "/user/edit/:id",
-  //         element: <ProfilePage />,
-  //         errorElement: <NotFound />,
-  //       },
-  //     ],
-  //   },
-  // ]);
-
   return (
     <React.StrictMode>
       <BrowserRouter>
