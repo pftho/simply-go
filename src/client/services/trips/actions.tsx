58cd4d3175cc3b0e9cdcae43b82427420dbf43a4
Pauline Thomas <p.thomas@wildsense.co>
2023-08-01 15:48:59 +0200

    add auth to all trips query

--- src/client/services/trips/actions.tsx
+++ src/client/services/trips/actions.tsx
@@ -21,18 +21,25 @@ export const useTripsQuery = (opt = {}) => {
 };
 
 export const useTripQuery = (id?: string, opt = {}) => {
+  const { getToken } = useAuth();
+
   return useQuery<Trip>({
     queryKey: ["trip", id],
-    queryFn: () =>
-      api.get(`/api/trips/${id}`).then((res) => {
-        return res.data;
-      }),
+    queryFn: async () =>
+      api
+        .get(`/api/trips/${id}`, {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        })
+        .then((res) => {
+          return res.data;
+        }),
     ...opt,
   });
 };
 
 export const useCreateSurveyMutation = () => {
   const { getToken } = useAuth();
+
   return useMutation<Trip, null, TripCreationRequest>(async (data) => {
     return await api
       .post("/api/trips", data, {
@@ -43,8 +50,14 @@ export const useCreateSurveyMutation = () => {
 };
 
 export const useUpdateTripMutation = () => {
+  const { getToken } = useAuth();
+
   return useMutation<Trip, null, { data: Partial<Trip>; id: string }>(
-    ({ data, id }) =>
-      api.patch(`/api/trips/${id}`, data).then((res) => res.data)
+    async ({ data, id }) =>
+      api
+        .patch(`/api/trips/${id}`, data, {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        })
+        .then((res) => res.data)
   );
 };
